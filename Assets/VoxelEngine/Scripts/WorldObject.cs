615dfd5a139c41af280f45f2ebd9f8cec77678ee
Mark Craig <mrmarkcraig@googlemail.com>
2016-12-09 06:52:44 +0000

    Started chunk object creation code

--- Assets/VoxelEngine/Scripts/WorldObject.cs
+++ Assets/VoxelEngine/Scripts/WorldObject.cs
@@ -9,9 +9,12 @@ namespace Voxel
     {
         public IntVec3 m_WorldSize = new IntVec3(16, 16, 16);
         public IntVec3 m_ChunkSize = new IntVec3(32, 32, 32);
+        public GameObject m_ChunkPrefab;
 
         private WorldData m_WorldData = new WorldData();
         private MeshBuilder m_MeshBuilder;
+
+        private GameObject m_ChunkRoot;
 
         // Use this for initialization
         void Start ()
@@ -23,7 +26,17 @@ namespace Voxel
             m_MeshBuilder = new MeshBuilder(m_WorldData);
             // Test
             m_WorldData.SetBlock(new IntVec3(0, 0, 0), BlockType.Solid);
+
+            CreateChunkObjects();
 	    }
+
+        void CreateChunkObjects()
+        {
+            m_ChunkRoot = new GameObject("ChunkRoot");
+            m_ChunkRoot.transform.parent = transform;
+
+            // TODO: build chunk objects under root from Prefab
+        }
 	
 	    // Update is called once per frame
 	    void Update () {
